8551f1af6d7517f303f4aba3b410ae9c5f077ce0
MariamIdrees <thumininu94@gmail.com>
2026-08-18 23:13:42 +0100

    second commit

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-let userAge = parseInt(prompt ("Hello, how old are you?"))
+// let userAge = parseInt(prompt ("Hello, how old are you?"))
 
-if (userAge <12  ){
-    alert("your ticket price is 5 dollars");
+// if (userAge <12  ){
+//     alert("your ticket price is 5 dollars");
 
-} 
+// } 
 
-else if (userAge < 18 ) {
-    alert ("your ticket price is 10 dollars");
-} else if (userAge < 60) {
-     alert  ("your ticket price is 20 dollars")}
+// else if (userAge < 18 ) {
+//     alert ("your ticket price is 10 dollars");
+// } else if (userAge < 60) {
+//      alert  ("your ticket price is 20 dollars")}
 
-     else {
-     alert  ("your ticket is 15 dollars")}
+//      else {
+//      alert  ("your ticket is 15 dollars")}
 
    
 
 
 
-let purchase = parseInt(prompt("Hello, How much are you purchasing?"))
-if (purchase >= 100 ){
-    alert("congratulations!!! you have a discount of 20 dollars")
-} else if (purchase >= 50){
-    alert ("congratulations!!! you have a discount of 10 dollars")
+// let purchase = parseInt(prompt("Hello, How much are you purchasing?"))
+// if (purchase >= 100 ){
+//     alert("congratulations!!! you have a discount of 20 dollars")
+// } else if (purchase >= 50){
+//     alert ("congratulations!!! you have a discount of 10 dollars")
 
- } else if (isNaN(purchase)){
-    alert ("you have to enter in numbers")
+//  } else if (isNaN(purchase)){
+//     alert ("you have to enter in numbers")
 
-}else  { alert ("sorry, you do not have any discount!")}
-
-
+// }else  { alert ("sorry, you do not have any discount!")}
 
 
+
+
+for (let  f = 2; f <= 12; f++){
+    for (g= 1; g <=12 ; g++) {
+let h = `${f}* ${g} = ${f*g};`
+
+console.log (h)
+    }
+}
