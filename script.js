8c334f31c8c9e4b9eeabe979c7ceac69af8984b5
MariamIdrees <thumininu94@gmail.com>
2026-08-19 20:42:04 +0100

    third commit

--- script.js
+++ script.js
@@ -31,10 +31,44 @@
 
 
 
-for (let  f = 2; f <= 12; f++){
-    for (g= 1; g <=12 ; g++) {
-let h = `${f}* ${g} = ${f*g};`
+// for (let  f = 2; f <= 12; f++){
+//     for (g= 1; g <=12 ; g++) {
+//         let h = f * g;
+//         if(h%2 !==0){
 
-console.log (h)
-    }
-}
+       
+// console.log( `${f}* ${g} = ${f*g}`);
+
+//     }
+//      }
+// }
+// for(x = 0; x <=20; x++){
+//     if(x%2 !==0){
+//         console.log(x)
+//     }
+// }
+
+// function Username(yourname){
+// console.log(yourname);
+// }
+// Username("Mariam")
+
+// const newAge = (YourAge) =>{
+//     console.log (YourAge)
+// } 
+
+// newAge (30)
+
+// let CirlceRadius = (Radius) =>{
+//     console.log(Math.PI * Radius * Radius)
+// }
+// CirlceRadius(7)
+// let b = parseInt(prompt("enter the value of b"))
+// let h = parseInt(prompt("enter the value of h"))
+// let AreaofTriangle = 0.5 * b * h
+// console.log("Area of a Triangle =" + AreaofTriangle)
+
+// let l = parseInt(prompt("enter lenght"))
+// let h = parseInt(prompt("enter width"))
+// let AreaofRectangle = l * h
+// console.log ("Area of a rectangle ="  + AreaofRectangle )
